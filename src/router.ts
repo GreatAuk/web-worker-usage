00bb35ac4944ead8c43be7c3d5449b1fdd518c4b
Utopia <greatauk11@gmail.com>
2022-10-01 20:56:51 +0800

    feat: update router base path

--- src/router.ts
+++ src/router.ts
@@ -14,8 +14,9 @@ const routes = [
   },
 ];
 
+const pathBase = import.meta.env.PROD ? "/web-worker-demo/" : undefined;
 export const router = createRouter({
   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-  history: createWebHistory(),
+  history: createWebHistory(pathBase),
   routes, // `routes: routes` 的缩写
 });
