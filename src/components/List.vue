eea6fa4c44b072fa66b9f26e64a157a1859e0c4e
Utopia <greatauk11@gmail.com>
2022-10-01 20:07:51 +0800

    feat: init project

---
+++ src/components/List.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const list = Array(1000).fill(null);
+</script>
+
+<template>
+  <ul>
+    <li v-for="(item, index) in list" :key="index">
+      {{ index }}
+    </li>
+  </ul>
+</template>
