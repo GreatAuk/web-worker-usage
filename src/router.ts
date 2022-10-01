cdf3b139ae69695269da851cb1524fa906955f22
Utopia <greatauk11@gmail.com>
2022-10-01 21:12:44 +0800

    feat: router use hash mode

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
   {
@@ -17,6 +17,6 @@ const routes = [
 const pathBase = import.meta.env.PROD ? "/web-worker-demo/" : undefined;
 export const router = createRouter({
   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-  history: createWebHistory(pathBase),
+  history: createWebHashHistory(pathBase),
   routes, // `routes: routes` 的缩写
 });
